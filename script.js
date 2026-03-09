7cd7d0864d31d96d62e7308ba98dc7c875e26883
kaderaid08 <karinar97@nycstudents.net>
2026-03-09 11:41:05 -0400

    weee

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("hello")
+const openAnimation = document.querySelector("#openAnimation");
+let flowerIcon = document.querySelector("#navICON");
+// let scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) 
+//  (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+        console.log("hiii")
+    }
+}
